eb04d051124c5490619cfae66e9853421befdb71
kebzin <kebbawaiga@gmail.com>
2025-04-01 17:50:57 +0000

    refactor app.js for consistent quotation marks and improved MongoDB connection handling

--- app.js
+++ app.js
@@ -1,134 +1,150 @@
 if (process.env.NODE_ENV !== "production") {
-    require('dotenv').config();
+  require("dotenv").config();
 }
 
 // console.log(process.env.SECRET)
 // console.log(process.env.API_KEY)
 
-const express = require('express');
+const express = require("express");
 // installation - npm install cors:  controls cross-origin requests security
-const cors = require('cors');
+const cors = require("cors");
 
 //installation - npm install cookie-parser: allows easy access to cookie data sent by clients
-const cookieParser = require('cookie-parser');
-const mongoose = require('mongoose');
-const path = require('path');
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+const path = require("path");
 //===npm i ejs-mate===//--> Simplifies layout management in Express apps.==//
-const ejsMate = require('ejs-mate');
+const ejsMate = require("ejs-mate");
 //===npm i express-session===//--> manages user sessions==//
 // stores session data on the server-side, typically using session ID in a cookie
 // on the client-side
-const session = require('express-session');
+const session = require("express-session");
 // to create mongoDB session store-installation--> npm install connect-mongo@latest
-const MongoStore = require('connect-mongo') //--> Allows to store session data in mongoDB database
+const MongoStore = require("connect-mongo"); //--> Allows to store session data in mongoDB database
 // npm i connect-flash -->storing  and displaying temporary messages between requests
-const flash = require('connect-flash');
-const { campgroundSchema, reviewSchema } = require('./schemas.js');
-const ExpressError = require('./Utils/ExpressError');
-const methodOverride = require('method-override');
+const flash = require("connect-flash");
+const { campgroundSchema, reviewSchema } = require("./schemas.js");
+const ExpressError = require("./Utils/ExpressError");
+const methodOverride = require("method-override");
 //== plugins for authentication
-const passport = require('passport');
-const LocalStrategy = require('passport-local');
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
 
-const User = require('./models/user')
-const Campground = require('./models/campground');
-const Reservation = require('./models/reservation')
-const Review = require('./models/review');
+const User = require("./models/user");
+const Campground = require("./models/campground");
+const Reservation = require("./models/reservation");
+const Review = require("./models/review");
 //=installation npm i helmet
-const helmet = require('helmet'); //enhancing security by setting various HTTP headers
+const helmet = require("helmet"); //enhancing security by setting various HTTP headers
 
 //preventing mongo injection attacks
-const mongoSanitize = require('express-mongo-sanitize');
+const mongoSanitize = require("express-mongo-sanitize");
 
-const userRoutes = require('./routes/users')
-const campgroundRoutes = require('./routes/campgrounds');
-const reservationRoutes = require('./routes/reservations.js');
-const carRoutes = require('./routes/cars')
-const productRoutes = require('./routes/Product')
-const tourRoutes = require('./routes/tours')
-const reviewRoutes = require('./routes/reviews');
+const userRoutes = require("./routes/users");
+const campgroundRoutes = require("./routes/campgrounds");
+const reservationRoutes = require("./routes/reservations.js");
+const carRoutes = require("./routes/cars");
+const productRoutes = require("./routes/Product");
+const tourRoutes = require("./routes/tours");
+const reviewRoutes = require("./routes/reviews");
 // const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
-const dbUrl = 'mongodb://localhost:27017/yelp-camp'
+const dbUrl =
+  "mongodb+srv://kebbawaiga:Howareyoudoing1@eventmanagement.uwtft.mongodb.net/?retryWrites=true&w=majority&appName=EventManagement";
 mongoose.connect(dbUrl);
+
+const connectDB = async () => {
+  try {
+    // console.log("mongo_uri: ", process.env.MONGO_URI);
+    const conn = await mongoose.connect(dbUrl);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log("Error connection to MongoDB: ", error.message);
+    process.exit(1); // 1 is failure, 0 status code is success
+  }
+};
+
+connectDB();
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
-    console.log("Database connected");
+  console.log("Database connected");
 });
 
 const app = express();
 
-app.engine('ejs', ejsMate)
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
-app.use(methodOverride('_method'));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(mongoSanitize({
-    replaceWith: '_'
-}));
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  mongoSanitize({
+    replaceWith: "_",
+  })
+);
 
 const corsOptions = {
-    origin: process.env.CLIENT_URL || 'http://localhost:3000',
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-    credentials: true
+  origin: process.env.CLIENT_URL || "http://localhost:3000",
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+  credentials: true,
 };
 
 app.use(cors(corsOptions));
 
 //==Server Side Validation==// related to JOI
 const validateCampground = (req, res, next) => {
-    const { error } = campgroundSchema.validate(req.body);
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400)
-    } else {
-        next();
-    }
-}
+  const { error } = campgroundSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(msg, 400);
+  } else {
+    next();
+  }
+};
 
 const validateReview = (req, res, next) => {
-    const { error } = reviewSchema.validate(req.body);
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400)
-    } else {
-        next();
-    }
-}
+  const { error } = reviewSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(msg, 400);
+  } else {
+    next();
+  }
+};
 
-const secret = process.env.SECRET || 'thisshouldbeabettersecret!'
+const secret = process.env.SECRET || "thisshouldbeabettersecret!";
 
 // storing sessions in our mongoDB database
 const store = MongoStore.create({
-    mongoUrl: dbUrl,
-    touchAfter: 24 * 60 * 60,
-    crypto: {
-        secret
-    }
+  mongoUrl: dbUrl,
+  touchAfter: 24 * 60 * 60,
+  crypto: {
+    secret,
+  },
 });
 
 store.on("error", function (e) {
-    console.log("SESSION STORE ERROR", e)
-})
+  console.log("SESSION STORE ERROR", e);
+});
 
 const sessionConfig = {
-    store,
-    name: 'session', //avoiding session spoofing
-    secret,
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-        httpOnly: true, // security feature that helps protect against cross-site scripting (XSS) attacks
-        secure: process.env.NODE_ENV === 'production',
-        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
-        maxAge: 1000 * 60 * 60 * 24 * 7
-    }
-}
+  store,
+  name: "session", //avoiding session spoofing
+  secret,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true, // security feature that helps protect against cross-site scripting (XSS) attacks
+    secure: process.env.NODE_ENV === "production",
+    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+  },
+};
 app.use(session(sessionConfig));
 app.use(flash());
 
@@ -166,7 +182,6 @@ app.use(flash());
 //     "https://cdnjs.cloudflare.com/"
 // ];
 
-
 // app.use(
 //     helmet.contentSecurityPolicy({
 //         directives: {
@@ -194,42 +209,42 @@ app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 
-passport.serializeUser(User.serializeUser())
+passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
-    res.locals.currentUser = req.user;
-    res.locals.success = req.flash('success');
-    res.locals.error = req.flash('error');
-    next();
-})
+  res.locals.currentUser = req.user;
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  next();
+});
 
-app.use('/', userRoutes)
-app.use('/campgrounds', campgroundRoutes)
-app.use('/reservations', reservationRoutes);
-app.use('/campgrounds/:id/reviews', reviewRoutes)
-app.use('/cars', carRoutes)
-app.use('/products', productRoutes)
-app.use('/tours', tourRoutes)
-app.get('/', (req, res) => {
-    //debugging steps to see if the route being accessed
-    // console.log("Home route accessed")
-    // res.send('home page is working')
-    res.render('home');
-})
+app.use("/", userRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/reservations", reservationRoutes);
+app.use("/campgrounds/:id/reviews", reviewRoutes);
+app.use("/cars", carRoutes);
+app.use("/products", productRoutes);
+app.use("/tours", tourRoutes);
+app.get("/", (req, res) => {
+  //debugging steps to see if the route being accessed
+  // console.log("Home route accessed")
+  // res.send('home page is working')
+  res.render("home");
+});
 
-app.all('*', (req, res, next) => {
-    next(new ExpressError('Page Not Found', 404))
-})
+app.all("*", (req, res, next) => {
+  next(new ExpressError("Page Not Found", 404));
+});
 
 // express error handler
 app.use((err, req, res, next) => {
-    console.error(err.stack); //to log the full error stack
-    const { statusCode = 500 } = err;
-    if (!err.message) err.message = 'Oh No, Something Went Wrong!'
-    res.status(statusCode).render('error', { err });
-})
+  console.error(err.stack); //to log the full error stack
+  const { statusCode = 500 } = err;
+  if (!err.message) err.message = "Oh No, Something Went Wrong!";
+  res.status(statusCode).render("error", { err });
+});
 
 app.listen(3000, () => {
-    console.log("SERVING ON PORT 3000!");
-})
+  console.log("SERVING ON PORT 3000!");
+});
